5905eeb86d4603a3d9a1335798a1eef4baa030ff
janlimjh <janlimjh@gmail.com>
2019-11-05 13:13:20 +0800

    v1 update

--- GoWhere/suspendUA.aspx.cs
+++ GoWhere/suspendUA.aspx.cs
@@ -23,6 +23,28 @@ public partial class suspendUA : System.Web.UI.Page
         con.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("/GoWhere/App_Data/Database.mdb");
         con.Open(); // connection open 
 
+        sql = "SELECT COUNT(*) FROM Tour_Guide WHERE user='" + suspendUser.Text + "'";
+        OleDbCommand cmd = new OleDbCommand(sql, con);
+        row = (int)cmd.ExecuteScalar(); // cast into integer and ExecuteScalar() get single value from database.
+        if (row > 0)
+        {
+            sql = "UPDATE Tour_Guide SET [suspended]=? WHERE user=?";
+            cmd = new OleDbCommand(sql, con);
+            cmd.Parameters.AddWithValue("?", true);
+            cmd.Parameters.AddWithValue("?", suspendUser.Text);
+            cmd.ExecuteNonQuery();
 
+            sql = "UPDATE Tourist SET [suspended]=? WHERE user=?";
+            cmd = new OleDbCommand(sql, con);
+            cmd.Parameters.AddWithValue("?", true);
+            cmd.Parameters.AddWithValue("?", suspendUser.Text);
+            cmd.ExecuteNonQuery();
+
+            Label2.Text = suspendUser.Text + " suspended";
+        }
+        else
+            Label2.Text = "User does not exist.";
+
+        con.Close();
     }
 }
